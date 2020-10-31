342c35b8751f6fcc8e010b17c857b8551296c546
DeLiGeRiCang <2533678748@qq.com>
2020-10-31 12:24:08 +0800

    首页-我的修改

--- pages/index/t1/t1.js
+++ pages/index/t1/t1.js
@@ -7,7 +7,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    datat1:[
+      {id:1,image:'../../../images/t1/tb01.png',text:"访客通行"},
+      {id:2,image:'../../../images/t1/tb02.png',text:"物业维修"},
+      {id:3,image:'../../../images/t1/tb03.png',text:"物业缴费"},
+      {id:4,image:'../../../images/t1/tb04.png',text:"生活缴费"},
+      {id:5,image:'../../../images/t1/tb05.png',text:"楼栋布局"},
+      {id:6,image:'../../../images/t1/tb06.png',text:"活动设施"},
+      {id:7,image:'../../../images/t1/tb07.png',text:"SOS求助"},
+      {id:8,image:'../../../images/t1/tb08.png',text:"查看全部"}
+    ]
   },
  //点击登录
  loginClick(){
