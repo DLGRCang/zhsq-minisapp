fce32bd7b8f9394883fca65d4716af3711afccfd
贺改英 <1639233637@163.com>
2020-11-03 11:15:18 +0800

    照片插件js补充

--- pages/wywx/wywx.js
+++ pages/wywx/wywx.js
@@ -38,6 +38,47 @@ Page({
   changeDateTime(e) {
     this.setData({ dateTime1: e.detail.value });
   },
-
+  // 照片功能
+  ChooseImage() {
+    wx.chooseImage({
+      count: 9, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        if (this.data.imgList.length != 0) {
+          this.setData({
+            imgList: this.data.imgList.concat(res.tempFilePaths)
+          })
+        } else {
+          this.setData({
+            imgList: res.tempFilePaths
+          })
+        }
+      }
+    });
+  },
+  ViewImage(e) {
+    wx.previewImage({
+      urls: this.data.imgList,
+      current: e.currentTarget.dataset.url
+    });
+  },
+  DelImg(e) {
+    wx.showModal({
+      title: '删除照片',
+      content: '确定要删除该照片吗？',
+      cancelText: '删除',
+      confirmText: '取消',
+      success: res => {
+        if (res.confirm) {
+          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+          this.setData({
+            imgList: this.data.imgList
+          })
+        }
+      }
+    })
+     },
+     
 })
 
