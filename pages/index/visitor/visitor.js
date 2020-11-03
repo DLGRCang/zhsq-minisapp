44219177274816d96cba23bbcdfd6d5b07bae345
DeLiGeRiCang <2533678748@qq.com>
2020-11-03 18:28:04 +0800

    修改路由布局

---
+++ pages/index/visitor/visitor.js
@@ -0,0 +1,88 @@
+// pages/visitor/visitor.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    Length:7,    //输入框个数 
+  isFocus:true,  //聚焦 
+  Value:"",    //输入的内容 
+  ispassword:false, //是否密文显示 true为密文， false为明文。
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  floorTui:function(){
+    wx.navigateBack({
+      delta: 1
+    })
+   },
+   Focus(e){ 
+    var that = this; 
+    console.log(e.detail.value);
+    var inputValue = e.detail.value; 
+    that.setData({ 
+     Value:inputValue, 
+    }) 
+   }, 
+   Tap(){ 
+    var that = this; 
+    that.setData({ 
+     isFocus:true, 
+    }) 
+   }, 
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
