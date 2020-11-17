d84d3b176123efc4858b018368870078fae7d9e2
hegaiying <1639233637@qq.com>
2020-11-17 15:49:09 +0800

    新增地址页面，加入地址控件

--- pages/index/news_address/news_address.js
+++ pages/index/news_address/news_address.js
@@ -5,9 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    region: ['内蒙古', '呼和浩特市', '新城区'],
   },
-
+  // 地址
+  RegionChange: function(e) {
+    this.setData({
+      region: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
