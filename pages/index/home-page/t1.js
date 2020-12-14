774d00800c725d8715ee664662f217367d3351c2
zhaoxueting <zxt2336@163.com>
2020-12-14 15:07:25 +0800

    1
    1

--- pages/index/home-page/t1.js
+++ pages/index/home-page/t1.js
@@ -1,4 +1,4 @@
-// pages/index/t1/t1.js
+// pages/index//.js
 import common from '../../../utils/common'
 import http from '../../../utils/api'
 import util from '../../../utils/util';
@@ -16,20 +16,20 @@ Component({
    */
   data: {
     dataItem:[
-      {id:1,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts1.png',text:"访客通行",url:'/pages/index/visitor/visitor'},
-      {id:2,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts2.png',text:"物业维修",url:'/pages/index/property-maintenance/wywx'},
-      {id:3,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts3.png',text:"物业缴费",url:'/pages/index/property-payment/wyjf'},
-      {id:4,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts4.png',text:"生活缴费",url:'/pages/index/living_payment/shjf'},
-      {id:5,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts5.png',text:"SOS求助",url:''},
-      {id:6,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts6.png',text:"房屋出租",url:'/pages/index/houseRental/fwcz'},
-      {id:7,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts7.png',text:"空中课堂",url:''},
-      {id:8,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts8.png',text:"视频直播",url:'/pages/index/liveBroadcast/liveBroadcast'},
-      //{id:9,image:'../../../images/t1/ts9.png',text:"党支部",url:'/pages/index/partyBranch/partyBranch'},
-      {id:10,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts10.png',text:"社区党建",url:'/pages/index/partyBuilding/partyBuilding'},
-      {id:11,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts11.png',text:"民情直达",url:'/pages/index/complaint/complaint'},
-      {id:12,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts12.png',text:"入驻小区",url:'/pages/index/checkIn/checkIn'},
-      {id:13,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts13.png',text:"楼栋布局",url:'/pages/index/floor/floor'},
-      {id:14,image:'https://yiqi.sucstep.com/zhsq/assets/images/applets/ts14.png',text:"活动设施",url:''}
+      {id:1,image:'../../../images/ts1.png',text:"访客通行",url:'/pages/index/visitor/visitor'},
+      {id:2,image:'../../../images/ts2.png',text:"物业维修",url:'/pages/index/property-maintenance/wywx'},
+      {id:3,image:'../../../images/ts3.png',text:"物业缴费",url:'/pages/index/property-payment/wyjf'},
+      {id:4,image:'../../../images/ts4.png',text:"生活缴费",url:'/pages/index/living_payment/shjf'},
+      {id:5,image:'../../../images/ts5.png',text:"SOS求助",url:''},
+      {id:6,image:'../../../images/ts6.png',text:"房屋出租",url:'/pages/index/houseRental/fwcz'},
+      {id:7,image:'../../../images/ts7.png',text:"空中课堂",url:''},
+      {id:8,image:'../../../images/ts8.png',text:"视频直播",url:'/pages/index/liveBroadcast/liveBroadcast'},
+      //{id:9,image:'../../../images/ts9.png',text:"党支部",url:'/pages/index/partyBranch/partyBranch'},
+      {id:10,image:'../../../images/ts10.png',text:"社区党建",url:'/pages/index/partyBuilding/partyBuilding'},
+      {id:11,image:'../../../images/ts11.png',text:"民情直达",url:'/pages/index/complaint/complaint'},
+      {id:12,image:'../../../images/ts12.png',text:"入驻小区",url:'/pages/index/checkIn/checkIn'},
+      {id:13,image:'../../../images/ts13.png',text:"楼栋布局",url:'/pages/index/floor/floor'},
+      {id:14,image:'../../../images/ts14.png',text:"活动设施",url:''}
     ],
     msgList: [
       { img: 'https://yiqi.sucstep.com/zhsq/assets/images/applets/index_banner.png' },
